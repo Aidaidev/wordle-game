20e6a46e253bf687089b47a4237d6d29b1ad491e
Aidaidev <bakytbekovnaa0100@gmail.com>
2024-06-15 16:14:37 +0600

    logica

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,85 +1,23 @@
 import React from "react";
+import "./Game.css";
 
-const Game = () => {
+interface GameProps {
+  index: number;
+  inputValue: string;
+  gameClass: string;
+}
+
+export const Game: React.FC<GameProps> = ({ index, inputValue, gameClass }) => {
   return (
-    <div id="game">
-      <div className="game-container">
-        <h1>Wordle</h1>
-        <div className="board" id="board">
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-          <div className="cell"></div>
-        </div>
-        <div className="keyboard">
-          <div className="row">
-            <button className="key">Q</button>
-            <button className="key">W</button>
-            <button className="key">E</button>
-            <button className="key">R</button>
-            <button className="key">T</button>
-            <button className="key">Y</button>
-            <button className="key">U</button>
-            <button className="key">I</button>
-            <button className="key">O</button>
-            <button className="key">P</button>
-          </div>
-          <div className="row">
-            <button className="key">A</button>
-            <button className="key">S</button>
-            <button className="key">D</button>
-            <button className="key">F</button>
-            <button className="key">G</button>
-            <button className="key">H</button>
-            <button className="key">J</button>
-            <button className="key">K</button>
-            <button className="key">L</button>
-          </div>
-          <div className="row">
-            <button className="key">ENTER</button>
-            <button className="key">Z</button>
-            <button className="key">X</button>
-            <button className="key">C</button>
-            <button className="key">V</button>
-            <button className="key">B</button>
-            <button className="key">N</button>
-            <button className="key">M</button>
-            <button className="key">⌫</button>
-          </div>
-        </div>
-        <div className="text">
-          <h2>Wordle #195</h2>
-          <h3>12/31/2021</h3>
-        </div>
-      </div>
+    <div className={`square ${gameClass}`} key={index}>
+      <input
+        placeholder="..."
+        type="text"
+        value={inputValue}
+        className="square_input"
+        readOnly
+        disabled
+      />
     </div>
   );
 };
-
-export default Game;
