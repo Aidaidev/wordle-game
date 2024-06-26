4be5816b70036ab538f818c6474839c5c1d44494
Aidaidev <bakytbekovnaa0100@gmail.com>
2024-06-26 14:41:46 +0600

    bug-fix

--- src/pages/Words.tsx
+++ src/pages/Words.tsx
@@ -44,9 +44,7 @@ const Words = () => {
               })}
             </div>
           ))}
-          {messsage && (
-            <p style={{ color: "white" }}>Правильное слово: {word}</p>
-          )}
+          {messsage && <p>Правильное слово: {word}</p>}
           {gameOver && (
             <div>
               <button onClick={initializeGame}>Начать новую игру</button>
